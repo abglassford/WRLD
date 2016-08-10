21f6e25bfb6d8ec5672d5a22557593cc8c7fd245
Alexander Glassford <a.b.glassford@gmail.com>
2016-08-10 16:39:57 -0600

    change map style

--- src/js/map-styles.js
+++ src/js/map-styles.js
@@ -4,17 +4,33 @@ var mapStyle = [
             { hue: '#F6E2A3' },
           ]
         },{
+          featureType: 'landscape',
+          stylers: [
+            { color: '#70573F' },
+            { saturation: -20 },
+            { lightness: 60 }
+          ]
+        },{
           featureType: 'all',
           stylers: [
             { saturation: -60 }
+          ]
+        },{
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [
+            { hue: '#F6E2A3' },
+            { saturation: -60 },
+            { lightness: -60 },
+            { visibility: 'simplified' }
           ]
         },{
           featureType: 'road.arterial',
           elementType: 'geometry',
           stylers: [
             { hue: '#F6E2A3' },
-            { saturation: 20 },
-            { lightness: -70 },
+            { saturation: 10 },
+            { lightness: -50 },
             { visibility: 'simplified' }
           ]
         },{
