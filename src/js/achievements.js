7165f2299722dcc9379c2c2a9407112d9b4bfaf8
Alexander Glassford <a.b.glassford@gmail.com>
2016-08-10 14:25:04 -0600

    add dynamic achievement functionality

---
+++ src/js/achievements.js
@@ -0,0 +1,40 @@
+function achivementFn (ach) {
+  ach.forEach(function(value){
+    if(value.requirement() === true){
+      if(!achLibrary.includes(achievements[0])){
+          achModal(achievements[0].name)
+          achLibrary.push(achievements[0])
+      }
+    }
+  })
+}
+function achModal(achievement) {
+  $('.modal-content').text(`Achievment Unlocked: ${achievement}!`)
+  $('.modal').modal('show')
+  setTimeout(function () {
+    $('.modal').modal('hide');
+  }, 3000)
+}
+
+
+var achievements = [
+  {
+    requirement: function () {return foundPlaces.length >= 20},
+    name: 'Baby Steps',
+    description: 'discover 20 places',
+    note: 'How do you eat an elephant? Why would you WANT to eat an elephant...?',
+    points: 10
+  }
+
+  //  {
+  //   name: `Barista's Bane`,
+  //   description: `discover 5 cafes`,
+  //   note: `The best part of waking up...`,
+  //   points: 5
+  // }, {
+  //   name: `Booze Hound`,
+  //   description: `discover 5 bars`,
+  //   note: `Sho Iwasfink'n... waiiit.. wawuzai shayhin?`,
+  //   points: 5
+  // }
+]
