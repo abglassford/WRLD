774139c2d6c8bcd5eb089b74d6d8da4b6e4fcd25
Alexander Glassford <a.b.glassford@gmail.com>
2016-08-11 10:42:26 -0600

    fix infowwindow error

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function initMap() {
     zoom: 18,
     styles: mapStyle
   });
-  infoWindow = new google.maps.InfoWindow();
+  infoWindow = new google.maps.InfoWindow({map: map});
   let customMarker = {
     url: './iconpack/icons/postal-code.svg',
     scaledSize: new google.maps.Size(40, 40)
